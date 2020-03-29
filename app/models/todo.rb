ce900b364d93317e121b66dcca24cf5be5152f8c
vamsi <vamsikrishna3415@outlook.in>
2020-03-29 14:53:59 +0530

    added scopes to query Todo table

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,10 @@
 class Todo < ActiveRecord::Base
+  scope :overdue, -> { where("due_date < ?", Date.today) }
+  scope :duetoday, -> { where("due_date = ?", Date.today) }
+  scope :duelater, -> { where("due_date > ?", Date.today) }
+  scope :completed, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
+
   def to_pleasant_string
     is_completed = completed ? "[X]" : "[]"
     "#{id}.#{due_date.to_s(:long)} #{todo_text} #{is_completed}"
