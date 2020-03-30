f799831f88edfe7153afd430526759d300956a1b
vamsi <vamsikrishna3415@outlook.in>
2020-03-30 19:40:27 +0530

    Updated db migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,8 @@
 class Todo < ActiveRecord::Base
-  scope :overdue, -> { where("due_date < ?", Date.today) }
+  scope :overdue, -> { where("due_date < ? and completed= ?", Date.today, false) }
   scope :duetoday, -> { where("due_date = ?", Date.today) }
   scope :duelater, -> { where("due_date > ?", Date.today) }
   scope :completed, -> { where(completed: true) }
-  scope :incomplete, -> { where(completed: false) }
 
   def to_pleasant_string
     is_completed = completed ? "[X]" : "[]"
