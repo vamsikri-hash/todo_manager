ab59d7816d9366b5644a41066d483d37fedce76e
vamsi <vamsikrishna3415@outlook.in>
2020-04-11 12:34:47 +0530

    Ability to signup for todo app using signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    User.create!(first_name: params[:first_name],
+                 last_name: params[:last_name],
+                 email: params[:email],
+                 password: params[:password])
+
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :todos
   resources :users
-  post "users/login", to: "users#login"
 end
