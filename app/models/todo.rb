75fa40e9bd61e799d833c9ba187f17f95307e46a
vamsi <vamsikrishna3415@outlook.in>
2020-04-11 11:40:02 +0530

    Ready with users table and relations with todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,11 +1,8 @@
 class Todo < ActiveRecord::Base
+  belongs_to :user
+
   scope :overdue, -> { where("due_date < ? and completed= ?", Date.today, false) }
   scope :duetoday, -> { where("due_date = ?", Date.today) }
   scope :duelater, -> { where("due_date > ?", Date.today) }
   scope :completed, -> { where(completed: true) }
-
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[]"
-    "#{id}.#{due_date.to_s(:long)} #{todo_text} #{is_completed}"
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
-  def to_pleasant_string
-    "#{id}.  #{name.titleize} , account created on #{created_at.to_s(:long)}"
-  end
+  has_many :todos
 end
