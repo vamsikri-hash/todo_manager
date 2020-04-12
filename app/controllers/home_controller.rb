51b570d62c22bfcefc5b44f4398055c0079ee2a4
vamsi <vamsikrishna3415@outlook.in>
2020-04-12 16:15:59 +0530

    Added sessions for user , routes for authentications

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def index
     render "index"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
@@ -6,7 +8,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     password = params[:password]
     if user && user.authenticate(password)
-      render plain: "correct password"
+      session[:current_user_id] = user.id
+      redirect_to todos_path
     else
       render plain: "Incorrect password"
     end
