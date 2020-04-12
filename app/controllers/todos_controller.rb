8e2e6c00ca2b1594203534dbfa34b7177af3ba6c
vamsi <vamsikrishna3415@outlook.in>
2020-04-12 21:44:04 +0530

    Fetch only currentuser todos and protection for update,delete todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,7 @@
 class TodosController < ApplicationController
+  before_action :find_current_todos
+
   def index
-    render "index"
   end
 
   def show
@@ -9,24 +10,29 @@ class TodosController < ApplicationController
   end
 
   def create
-    todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    Todo.create!(todo_text: params[:todo_text],
+                 due_date: params[:due_date],
+                 completed: false,
+                 user_id: current_user.id)
     redirect_to todos_path
   end
 
   def update
-    id = params[:id]
-    completed = params[:completed].nil? ? false : true
     # double bang operator is an aletrnative !!completed
-    todo = Todo.update(id, completed: completed)
+    completed = params[:completed].nil? ? false : true
+    @todos.update(params[:id], completed: completed)
     redirect_to todos_path
   end
 
   def destroy
-    id = params[:id]
-    todo = Todo.find(id)
+    todo = @todos.find(params[:id])
     todo.destroy
     redirect_to todos_path
+  end
+
+  private
+
+  def find_current_todos
+    @todos = current_user.todos
   end
 end
