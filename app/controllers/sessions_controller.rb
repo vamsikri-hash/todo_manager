cd825bd0755cf1e5d14936423e1d6490d29e3542
vamsi <vamsikrishna3415@outlook.in>
2020-04-13 17:57:21 +0530

    maintained contsant erb styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,6 +25,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:current_user_id] = nil
     @current_user = nil
+    flash[:success] = "Successfully logged out..!"
     redirect_to root_path
   end
 end
